e876ce2ab003099f93d355ebcd9848f3b67dde05
Samuel.Park <samuel@floatic.io>
2024-07-11 09:48:42 +0900

    chore : init

---
+++ src/main/kotlin/org/project/portfolio/PortfolioApplication.kt
@@ -0,0 +1,11 @@
+package org.project.portfolio
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PortfolioApplication
+
+fun main(args: Array<String>) {
+    runApplication<PortfolioApplication>(*args)
+}
