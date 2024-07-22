1ee85749e1417b1a934035f2b637d42f01a0f93b
KROOKIMWOOBIN <woodol4540@naver.com>
2024-07-22 19:29:25 +0900

    FEAT: ìì½코틀린에서 자바로 변경

---
+++ src/test/java/org/project/ApplicationTests.java
@@ -0,0 +1,12 @@
+package org.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
