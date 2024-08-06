779380d0aa831cbe54ee38e0b8c766039a6a9bfa
ycyeon <ickite@naver.com>
2024-08-06 22:40:36 +0900

    chore : 프로젝트 구조 변경

---
+++ src/main/java/org/project/portfolio/PortfolioApplication.java
@@ -0,0 +1,11 @@
+package org.project.portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PortfolioApplication.class, args);
+    }
+}
---
+++ src/test/java/org/project/portfolio/PortfolioApplicationTests.java
@@ -0,0 +1,12 @@
+package org.project.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
